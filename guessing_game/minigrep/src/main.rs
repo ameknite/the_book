85a7ab2d5603e66910f8fa76700ff606bed7619e
atinyame <104745335+atinyame@users.noreply.github.com>
2022-08-04 17:59:37 -0500

    Returning Result from new Instead of Calling panic! Calling Config::new and Handling Errors

--- guessing_game/minigrep/src/main.rs
+++ guessing_game/minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,13 +23,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn new(args: &[String]) -> Result<Self, &str> {
         if args.len() < 3 {
-            panic!("not enough arguments")
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Self { query, filename }
+        Ok(Self { query, filename })
     }
 }
