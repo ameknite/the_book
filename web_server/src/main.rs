be55db29d95759003aab6c97c4386e3b141db043
atinyame <104745335+atinyame@users.noreply.github.com>
2022-08-05 12:46:43 -0500

    Simulating a Slow Request in the Current Server

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,7 +1,7 @@
-use std::fmt::format;
-use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,9 +18,14 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) = match buffer.starts_with(get) {
-        true => ("HTTP/1.1 200 OK", "hello.html"),
+    let (status_line, filename) = match buffer {
+        buffer if buffer.starts_with(get) => ("HTTP/1.1 200 OK", "hello.html"),
+        buffer if buffer.starts_with(sleep) => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
         _ => ("HTTP/1.1 200 NOT FOUND", "404.html"),
     };
 
