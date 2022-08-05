86cff8c6072b2c5bd22c737c0facd6da1a5125f6
atinyame <104745335+atinyame@users.noreply.github.com>
2022-08-05 17:59:49 -0500

    Signaling to the Threads to Stop Listening for Jobs

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -5,10 +5,15 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -39,12 +44,20 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -61,13 +74,20 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {id} got a job; executing");
-
-            job();
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {id} was told to terminate.");
+                    break;
+                }
+            }
         });
 
         Self {
